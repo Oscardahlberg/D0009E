9e5c405ee1ed7ad9dd0485db8528d572799842a5
Oscardahlberg <31594957+Oscardahlberg@users.noreply.github.com>
2019-09-29 09:57:18 +0200

    Fixed bugg

--- Labb3Uppgift1b/dictionaryTuple.py
+++ Labb3Uppgift1b/dictionaryTuple.py
@@ -38,30 +38,29 @@ with open("ordlista1.txt", "a+") as f:
         position_ord = 0
 
     while True:
-        print("Menu for dictionary")
-        print("1: Insert")
-        print("2: Lookup")
-        print("3: Exit Program")
-        user_choice = input()
-        if user_choice == "3":
-            break
-        if user_choice == "2":
-            print("Word to lookup: ")
-            user_input = input()
-            m = 0
-            while True:
-                if user_input == list_of_tuples_of_words[m][0]:
-                    print("Description for " + user_input + ": " + list_of_tuples_of_words[m][1] + "\n")
-                    break
-                m += 1
-                if m == len(list_of_tuples_of_words):
-                    print("Word not found pepehands")
-                    break
-        if user_choice == "1":
-            print("Word to insert: ")
-            temporary[0][0] = input()
-            print("Description of word: ")
-            temporary[0][1] = input()
-            list_of_tuples_of_words.append(temporary[0])
-            f.write("\n" + temporary[0][0] + " " + temporary[0][1] + "1")
-            print("Word added, type '3' to save the word for future use")
+    print("Menu for dictionary")
+    print("1: Insert")
+    print("2: Lookup")
+    print("3: Exit Program")
+    user_choice = input()
+    if user_choice == "3":
+        break
+    if user_choice == "2":
+        print("Word to lookup: ")
+        user_input = input()
+        m = 0
+        while True:
+            if user_input == list_of_tuples[m][0]:
+                print("Description for " + list_of_tuples[m][0] + ": " + list_of_tuples[m][1] + "\n")
+                break
+            m += 1
+            if m == len(list_of_tuples):
+                print("Word not found")
+                break
+    if user_choice == "1":
+        print("Word to insert: ")
+        word = input()
+        print("Description of word: ")
+        definition = input()
+        list_of_tuples.append([word, definition])
+        print("Word added")
